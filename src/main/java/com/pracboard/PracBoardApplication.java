7c769f2f304170fb6d2d5cd898ca430bbac04a66
KTKT <a-b-c@kakao.com>
2022-07-17 20:38:16 +0900

    #1 pracBoard

---
+++ src/main/java/com/pracboard/PracBoardApplication.java
@@ -0,0 +1,13 @@
+package com.pracboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracBoardApplication.class, args);
+    }
+
+}
